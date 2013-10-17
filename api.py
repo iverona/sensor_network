97947d07d491d433e67da114c19eb5b0d2ee0b6b
Ignacio Verona <ignacio.verona@blue-tc.com>
2013-10-17 21:52:43 +0200

    Added support for WSGI

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import subprocess, os, shlex
+import bottle
 from bottle import route, run, post, request
 
 @post('/sensor')
@@ -7,16 +8,18 @@ def sensor():
 
 	for token in sensor_data.split():
 		try:
-			print float(token)
 			loadValue(float(token))
 		except ValueError:
 			pass
 
 def loadValue(value):
-	print "Loading value..."
+	print "Loading value... %f" % value
 	devnull = open(os.devnull, 'w')
 	subprocess.call(shlex.split(
 			'/usr/bin/zabbix_sender -z 127.0.0.1 -s TempSensor -k sensors.temp -o %s' % str(value-1.3)),
 			stdout=devnull, stderr=devnull) 
 
-run(host='0.0.0.0',port=8080, debug=True)
+if __name__ == "__main__":
+	run(host='0.0.0.0',port=8080, debug=True)
+
+app = bottle.default_app()
